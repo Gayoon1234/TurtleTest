7180985885bfd2635c0ee369018efeac656fbea2
Gayoon1234 <s3843584@student.rmit.edu.au>
2026-08-25 00:31:57 +1000

    feat(): add more snake

--- turtles/snake.py
+++ turtles/snake.py
@@ -1,40 +1,55 @@
-import random
 import turtle
-from constants import ROYAL_COLORS, Color
+
+from constants import Color
 from utils.screen_helper import setup_screen
+
 
 def run_snake_game(screen=None):
     if screen is None:
-        screen = setup_screen("snake.py - Turtle Animation", draw_instantly=False)
+        screen = setup_screen(
+            "snake.py - Turtle Animation",
+            draw_instantly=False,
+            bg_color="black"
+        )
 
     snake = turtle.Turtle()
+    snake.shape("square")
     snake.color(Color.GREEN.value)
-    snake.hideturtle()
-    snake.speed('fastest')
-    snake.shapesize(stretch_wid=10, stretch_len=10)
-    snake.forward(20)
+    snake.penup()
+    snake.speed("fastest")
 
-    def up():
-        snake.setheading(90)
-        snake.forward(20)
+    segments = [snake]
 
-    def down():
-        snake.setheading(270)
-        snake.forward(20)
+    for position in [(20, 0), (40, 0), (60, 0), (80, 0)]:
+        segment = turtle.Turtle()
+        segment.shape("square")
+        segment.color(Color.GREEN.value)
+        segment.penup()
+        segment.goto(position)
+        segment.speed("fastest")
+        segments.append(segment)
 
-    def left():
-        snake.setheading(180)
-        snake.forward(20)
-
-    def right():
-        snake.setheading(0)
-        snake.forward(20)
+    def recolor_snake(color):
+        for segment in segments:
+            segment.color(color)
 
     screen.listen()
-    screen.onkey(up, "Up")
-    screen.onkey(down, "Down")
-    screen.onkey(left, "Left")
-    screen.onkey(right, "Right")
-    
+    screen.onkey(lambda: snake.setheading(90), "Up")
+    screen.onkey(lambda: snake.setheading(270), "Down")
+    screen.onkey(lambda: snake.setheading(180), "Left")
+    screen.onkey(lambda: snake.setheading(0), "Right")
+    screen.onkey(lambda: recolor_snake(Color.RED.value), "1")
+    screen.onkey(lambda: recolor_snake(Color.GREEN.value), "2")
+    screen.onkey(lambda: recolor_snake(Color.BLUE.value), "3")
 
+    def move():
+        for i in range(len(segments) - 1, 0, -1):
+            segments[i].goto(segments[i - 1].position())
+
+        snake.forward(20)
+
+        screen.update()
+        screen.ontimer(move, 16)
+
+    move()
     screen.mainloop()
--- utils/screen_helper.py
+++ utils/screen_helper.py
@@ -1,8 +1,9 @@
 import turtle
 from constants import SHOULD_DRAW_INSTANTLY
 
-def setup_screen(title="My Turtle Project", draw_instantly=SHOULD_DRAW_INSTANTLY):
+def setup_screen(title="My Turtle Project", draw_instantly=SHOULD_DRAW_INSTANTLY, bg_color="black"):
     screen = turtle.Screen()
     screen.title(title)
     screen.tracer(0 if draw_instantly else 1)
+    screen.bgcolor(bg_color)
     return screen
